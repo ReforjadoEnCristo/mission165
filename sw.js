c92f2f8cb942b964f55759605263c796ed00199b
ReforjadoEnCristo <dsenanm72@gmail.com>
2026-08-30 10:47:44 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE = "m165-v5";
+var CACHE = "m165-v7";
 var ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png", "./apple-touch-icon.png"];
 self.addEventListener("install", function (e) {
   e.waitUntil(
